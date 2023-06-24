fcb1de4947d1b4399e73ceed014c9ad32ce0a9b4
kumastry <chma19063@g.nihon-u.ac.jp>
2023-06-25 04:17:24 +0900

    moocデータ作成用のコードを作成

---
+++ python/make_mooc_data.py
@@ -0,0 +1,55 @@
+import csv
+import json
+
+start = 1
+end = 10000 + 1
+
+mooc_data = []
+with open('public/act-mooc/mooc_actions.tsv', encoding='utf-8', newline='') as f:
+
+    for cols in csv.reader(f, delimiter='\t'):
+        mooc_data.append(cols)
+    f.close()
+
+userId_list = []
+targetId_list = []
+
+for i in range(start, end):
+    userId = int(mooc_data[i][1])
+    targetId = int(mooc_data[i][2])
+
+    userId_list.append(userId)
+    targetId_list.append(targetId)
+    print(userId, targetId)
+
+maxuserId = max(userId_list)
+minuserId = min(userId_list)
+
+maxtargetId = max(targetId_list)
+mintargetId = min(targetId_list)
+
+print("================================")
+print(maxuserId, minuserId)
+print(maxtargetId, mintargetId)
+
+matrix = [[0] * (maxtargetId - mintargetId + 1) for i in range(maxuserId - minuserId + 1)]
+
+for user, target in zip(userId_list, targetId_list):
+    matrix[user][target] = 1
+cnt = 0
+for i in matrix:
+    for j in i:
+        if(j==1):
+            cnt += 1
+
+print(cnt)
+print(len(userId_list))
+print(matrix)
+print(json.dumps(matrix))
+
+
+with open('public/act-mooc/json/mooc_actions_{}.json'.format(end-1), 'w') as f:
+    json.dump(matrix, f)
+
+
+
