407014b669e74ffbbd4d2bc0c49f84747a6cc08c
kumastry <chma19063@g.nihon-u.ac.jp>
2023-06-25 08:03:12 +0900

    取引データの修正

--- python/extract_transaction.py
+++ python/extract_transaction.py
@@ -9,12 +9,12 @@ with open('public/retailrocket/events.csv', encoding='utf-8', newline='') as f:
     for cols in csv.reader(f):
         if(cols[2] == 'transaction'):
             trans_cnt += 1
-            transactions.append({'visitorid':cols[1], 'itemid':cols[3]})
+            transactions.append({'visitorid':cols[1], 'itemid':cols[3], 'transactionid':cols[4]})
     f.close()
 print(trans_cnt)
 
 with open('public/retailrocket/transactions.csv', 'w',encoding='utf-8') as f:
-    writer = csv.DictWriter(f, ['visitorid', 'itemid'])
+    writer = csv.DictWriter(f, ['visitorid', 'itemid','transactionid'])
     writer.writeheader()
     for row in transactions:
         writer.writerow(row)
