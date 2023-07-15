7fa937818af91fcb04306c9a8295f0b6b27c75cf
kumastry <chma19063@g.nihon-u.ac.jp>
2023-07-16 05:35:11 +0900

    randomデータの作成スクリプト

---
+++ python/make_random_data.py
@@ -0,0 +1,27 @@
+#jsonのフォーマット
+#random_{left_num}_{right_num}_{probability}_{}.json
+import sys
+import random
+
+left_num = int(input())
+right_num = int(input())
+probability = int(input())
+file_num = int(input())
+
+for version in range(file_num):
+    output = []
+
+    for i in range(left_num):
+        vec = []
+        for j in range(right_num):
+            if(100*random.random() < probability):
+                vec.append(1)
+            else:
+                vec.append(0)
+        output.append(vec)
+
+    output_file = "public/random/json/random_{}_{}_{}_{}.json".format(left_num, right_num, probability, version+1)
+
+    with open(output_file, "w") as file:
+        sys.stdout = file  # 標準出力をファイルに変更します
+        print(output)
