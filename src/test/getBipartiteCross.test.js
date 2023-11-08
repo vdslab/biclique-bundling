c604accdaef5bece32f0d932ebc4bdbdf0c4f89e
kumastry <chma19063@g.nihon-u.ac.jp>
2023-11-09 02:00:57 +0900

    テストを修正した

--- src/test/getBipartiteCross.test.js
+++ src/test/getBipartiteCross.test.js
@@ -2,6 +2,7 @@ import { expect, test } from "vitest";
 import getBipartiteCross from "./../utils/getBipartiteCross";
 
 test("二部グラフの交差", () => {
+  // "public/random/json/random_7_7_75_2.json"
   const bipartite1 = [
     [1, 0, 0, 0, 0, 0],
     [1, 1, 0, 0, 0, 0],
@@ -11,25 +12,29 @@ test("二部グラフの交差", () => {
     [1, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 1],
   ];
+  const leftNodesOrder1 = [0, 1, 2, 3, 4, 5, 6];
+  const rightNodesOrder1 = [1, 0, 2, 3, 4, 5];
 
-  const bipartite2 = [
-    [1, 1, 0, 0, 0, 0, 0, 0],
+  //   const bipartite5 = [
+  //     [1, 1, 0, 0, 0, 0, 0, 0],
 
-    [1, 0, 0, 0, 0, 0, 0, 0],
+  //     [1, 0, 0, 0, 0, 0, 0, 0],
 
-    [1, 0, 1, 0, 0, 0, 0, 0],
+  //     [1, 0, 1, 0, 0, 0, 0, 0],
 
-    [0, 0, 0, 1, 0, 0, 0, 0],
+  //     [0, 0, 0, 1, 0, 0, 0, 0],
 
-    [0, 0, 0, 0, 1, 0, 0, 0],
+  //     [0, 0, 0, 0, 1, 0, 0, 0],
 
-    [0, 0, 0, 0, 0, 1, 0, 0],
+  //     [0, 0, 0, 0, 0, 1, 0, 0],
 
-    [0, 0, 0, 0, 0, 0, 1, 0],
-    [0, 0, 0, 0, 0, 0, 0, 1],
-  ];
+  //     [0, 0, 0, 0, 0, 0, 1, 0],
+  //     [0, 0, 0, 0, 0, 0, 0, 1],
+  //   ];
+  const leftNodesOrder4 = [5, 0, 2, 1, 4, 6, 3];
+  const rightNodesOrder4 = [6, 0, 1, 3, 2, 5, 7, 4];
 
-  const bipartite3 = [
+  const bipartite4 = [
     [1, 1, 0, 0, 0, 0, 0, 0],
     [0, 1, 1, 0, 0, 0, 0, 0],
     [0, 0, 0, 1, 0, 0, 0, 0],
@@ -39,7 +44,7 @@ test("二部グラフの交差", () => {
     [0, 0, 0, 0, 0, 0, 0, 1],
   ];
 
-  const bipartite4 = [
+  const bipartite2 = [
     [0, 1, 1, 1, 0],
     [1, 0, 0, 0, 0],
     [0, 0, 0, 0, 1],
@@ -47,9 +52,17 @@ test("二部グラフの交差", () => {
     [1, 0, 0, 0, 0],
     [0, 1, 0, 0, 0],
   ];
+  const leftNodesOrder2 = [1, 0, 2, 3, 4, 5];
+  const rightNodesOrder2 = [3, 0, 2, 4, 1];
 
-  expect(getBipartiteCross(bipartite1)).toBe(4);
-  expect(getBipartiteCross(bipartite2)).toBe(2);
-  expect(getBipartiteCross(bipartite3)).toBe(0);
-  expect(getBipartiteCross(bipartite4)).toBe(14);
+  expect(getBipartiteCross(bipartite1, leftNodesOrder1, rightNodesOrder1)).toBe(
+    4
+  );
+  expect(getBipartiteCross(bipartite2, leftNodesOrder2, rightNodesOrder2)).toBe(
+    8
+  );
+  expect(getBipartiteCross(bipartite4, leftNodesOrder4, rightNodesOrder4)).toBe(
+    1
+  );
+  //   expect(getBipartiteCross(bipartite5)).toBe(0);
 });
