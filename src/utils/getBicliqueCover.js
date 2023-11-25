7796b6741b0dcbae2892985f984ce3ca5c2a7d2a
kumastry <chma19063@g.nihon-u.ac.jp>
2023-11-26 02:24:50 +0900

    RLFの改良

--- src/utils/getBicliqueCover.js
+++ src/utils/getBicliqueCover.js
@@ -36,8 +36,6 @@ export const convertG2Ge = (G) => {
       if (G[iG + rowDiff][jG] && G[iG][jG + colDiff]) continue;
 
       //通過した(iG, jG)と(iGe, jGe)は結ぶ
-      console.log(iG, jG, iGe, jGe);
-      console.log(edge2Node.get(iG + "," + jG), edge2Node.get(iGe + "," + jGe));
       Ge[edge2Node.get(iG + "," + jG)].push(edge2Node.get(iGe + "," + jGe));
       Ge[edge2Node.get(iGe + "," + jGe)].push(edge2Node.get(iG + "," + jG));
     }
@@ -48,6 +46,115 @@ export const convertG2Ge = (G) => {
   return [Ge, edge2Node];
 };
 
+export const RLF = (G) => {
+  console.log("start");
+  if (!Object.entries(G).length) return;
+  console.log(G);
+  console.log("YES");
+  const S = [];
+  let maxDegNode = -1;
+  let maxNum = -1;
+  for (const [key, neighbors] of Object.entries(G)) {
+    console.log("huhu", key, neighbors);
+    if (neighbors.length > maxNum) {
+      maxDegNode = Number(key);
+      maxNum = neighbors.length;
+    }
+  }
+
+  //vertex-disjointを選ぶ
+  console.log("max degree", maxDegNode);
+  //maxDegNode = 0;
+  const U1 = new Set();
+  const U2 = new Set();
+  S.push(maxDegNode);
+
+  for (let i = 0; i < S.length; i++) {
+    for (const u2 of G[S[i]]) {
+      U2.add(u2);
+    }
+  }
+
+  console.log("U2", U2);
+  for (const [key] of Object.entries(G)) {
+    if (U2.has(Number(key)) || maxDegNode === Number(key)) continue;
+    U1.add(Number(key));
+  }
+
+  console.log("U1", U1);
+  //U1から選ぶでSに入れる；
+
+  while (U1.size) {
+    // U2にmaxの接続で選ぶ
+    let maxCount = -1;
+    const candObj = [];
+    const cand = [];
+    for (const u1Element of U1) {
+      let count = 0;
+      for (const node of G[u1Element]) {
+        if (U2.has(node)) {
+          count++;
+        }
+      }
+
+      maxCount = Math.max(maxCount, count);
+      candObj.push({ u1: u1Element, count });
+    }
+
+    candObj.forEach((element) => {
+      if (element.count === maxCount) {
+        cand.push(element.u1);
+      }
+    });
+    console.log("cand", cand, candObj);
+    console.log("end");
+
+    //candからU1に最小に繋がっているやつを選ぶ。
+    let minU1deg = 1e12;
+    let disi;
+    for (const cElement of cand) {
+      let count = 0;
+      for (const n of G[cElement].length) {
+        if (U1.has(n)) {
+          count++;
+        }
+      }
+
+      if (minU1deg > count) {
+        minU1deg = count;
+        disi = cElement;
+      }
+    }
+
+    console.log("disi", disi);
+    S.push(disi);
+    U1.delete(disi);
+
+    for (const n of G[disi]) {
+      U2.add(n);
+      U1.delete(n);
+    }
+
+    //console.log("S", S);
+    //console.log("U1", U1);
+  }
+
+  console.log("S", S);
+
+  //グラフからSを取り除く
+  for (const s of S) {
+    delete G[s];
+    for (let [key, nei] of Object.entries(G)) {
+      //console.log(key, s, nei);
+      nei = nei.filter((ele) => ele !== s);
+      //console.log(key, s, nei);
+      G[key] = nei;
+      //console.log("##########");
+    }
+  }
+
+  return S;
+};
 export const getBicliqueCover = (g) => {
   const [G, edge2Node] = convertG2Ge(g);
   const SS = [];
@@ -75,49 +182,68 @@ export const getBicliqueCover = (g) => {
       }
     }
 
-    console.log(U2);
+    console.log("U2", U2);
     for (const [key] of Object.entries(G)) {
       if (U2.has(Number(key)) || maxDegNode === Number(key)) continue;
       U1.add(Number(key));
     }
 
-    console.log(U1);
+    console.log("U1", U1);
     //U1から選ぶでSに入れる；
 
     while (U1.size) {
       // U2にmaxの接続で選ぶ
       let maxCount = -1;
+      const candObj = [];
       const cand = [];
-      for (const u1 of U1) {
+      for (const u1Element of U1) {
         let count = 0;
-        for (const node of G[u1]) {
-          if (!U2.has(node)) continue;
-          count++;
-        }
-        if (count >= maxCount) {
-          cand.push(u1);
-          maxCount = count;
-        } else {
-          while (!cand.length) {
-            cand.pop();
+        for (const node of G[u1Element]) {
+          if (U2.has(node)) {
+            count++;
           }
         }
+
+        maxCount = Math.max(maxCount, count);
+        candObj.push({ u1: u1Element, count });
       }
 
-      console.log("cand", cand);
-      //candを並び替える
+      candObj.forEach((element) => {
+        if (element.count === maxCount) {
+          cand.push(element.u1);
+        }
+      });
+      console.log("cand", cand, candObj);
+      console.log("end");
 
-      for (const c of cand) {
-        S.push(c);
-        U1.delete(c);
+      //candからU1に最小に繋がっているやつを選ぶ。
+      let minU1deg = 1e12;
+      let disi;
+      for (const cElement of cand) {
+        let count = 0;
+        for (const n of G[cElement]) {
+          if (U1.has(n)) {
+            count++;
+          }
+        }
 
-        for (const n of G[c]) {
-          U2.add(n);
+        if (minU1deg > count) {
+          minU1deg = count;
+          disi = cElement;
         }
       }
 
-      console.log("S", S);
-      console.log("U1", U1);
+      console.log("disi", disi);
+      S.push(disi);
+      U1.delete(disi);
+
+      for (const n of G[disi]) {
+        U2.add(n);
+        U1.delete(n);
+      }
+
+      //console.log("S", S);
+      //console.log("U1", U1);
     }
 
     console.log("S", S);
@@ -167,6 +293,7 @@ export const getBicliqueCover = (g) => {
     res.push(obj);
   }
 
+  console.log("SS", SS);
   console.error("res", res);
   return res;
 };
