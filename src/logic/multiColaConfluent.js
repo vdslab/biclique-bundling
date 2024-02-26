8b747fc5e4936f01408db868b3b64a1beb164251
kumastry <chma19063@g.nihon-u.ac.jp>
2024-02-27 02:17:48 +0900

    リファクタリング

--- src/logic/multiColaConfluent.js
+++ src/logic/multiColaConfluent.js
@@ -3,7 +3,7 @@ import * as cola from "webcola";
 import Confluent from "./../utils/confluent.js";
 import { getQuasiBicliqueCover } from "./../utils/getBicliqueCover.js";
 import getMissingEdgeColors from "./../utils/getMissingEdgeColors.js";
-import filterSameNodes from "./../utils/filterSameNodes.js";
+import makeGraphForCola from "./../utils/makeGraphForCola.js";
 import { getColaBipartiteCross } from "./../utils/getBipartiteCross.js";
 
 const colaConfluent = (bipartite, param, maxDepth, hasEdgeColor = false) => {
@@ -46,88 +46,7 @@ const colaConfluent = (bipartite, param, maxDepth, hasEdgeColor = false) => {
     - ノード、エッジ、制約が適切に入っているか？
     - 目視で確認した方が早い
   */
-  const graphEdges = new Array();
-  const graphNodesSet = new Set();
-  let pad = 0;
-  for (let k = 0; k < cf.bipartites.length; k++) {
-    const bipartite = cf.bipartites[k].bipartite;
-    for (let i = 0; i < bipartite.length; i++) {
-      for (let j = 0; j < bipartite[i].length; j++) {
-        if (!bipartite[i][j]) continue;
-        graphEdges.push({
-          source: i + pad,
-          target: j + pad + bipartite.length,
-        });
-        graphNodesSet.add({ id: i + pad, label: i, layer: k });
-        graphNodesSet.add({
-          id: j + pad + bipartite.length,
-          label: j,
-          layer: k + 1,
-        });
-      }
-    }
-    pad += bipartite.length;
-  }
-
-  const graphNodes = filterSameNodes(Array.from(graphNodesSet));
-
-  // グラフの制約を追加
-  const graphConstraints = new Array();
-  let idx = 0;
-  let prvIdx = 0;
-  let curIdx = 0;
-  for (let k = 0; k < cf.bipartites.length; k++) {
-    const leftNodesNum = cf.bipartites[k].bipartite.length;
-    const rightNodesNum = cf.bipartites[k].bipartite[0].length;
-    const constraint = { type: "alignment", axis: "y" };
-    const offsets = new Array();
-
-    // 一番上
-    if (k === 0) {
-      const toffsets = new Array();
-      const tconstraint = { type: "alignment", axis: "y" };
-      for (let i = 0; i < leftNodesNum; i++) {
-        toffsets.push({ node: String(idx++), offset: "0" });
-      }
-
-      tconstraint.offsets = toffsets;
-      graphConstraints.push(tconstraint);
-
-      curIdx = idx;
-      graphConstraints.push({
-        axis: "y",
-        left: prvIdx,
-        right: curIdx,
-        gap: 100,
-        equality: "true",
-      });
-      prvIdx = curIdx;
-    }
-
-    // 中央
-    for (let i = 0; i < rightNodesNum; i++) {
-      offsets.push({ node: String(idx++), offset: String(k + 1) });
-    }
-    constraint.offsets = offsets;
-    graphConstraints.push(constraint);
-
-    // 一番下
-    if (k === cf.bipartites.length - 1) continue;
-    curIdx = idx;
-    graphConstraints.push({
-      axis: "y",
-      left: prvIdx,
-      right: curIdx,
-      gap: 100,
-      equality: "true",
-    });
-    prvIdx = curIdx;
-  }
-
-  const graph = new Object();
-  graph.nodes = graphNodes;
-  graph.edges = graphEdges;
-  graph.constraints = graphConstraints;
+  const graph = makeGraphForCola(cf);
   // console.log(graph);
 
   // 座標決定process
