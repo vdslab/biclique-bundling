f7cdaf750907574f27576a783fa32859412e8d01
kumastry <chma19063@g.nihon-u.ac.jp>
2024-09-05 18:00:36 +0900

    add weighted bary-centric method data

--- src/logic/colaConfluent.js
+++ src/logic/colaConfluent.js
@@ -3,11 +3,10 @@ import * as cola from "webcola";
 import Confluent from "./../utils/confluent.js";
 import { getQuasiBicliqueCover } from "./../utils/getBicliqueCover.js";
 import { makeGraphForCola } from "./../utils/makeGraphForCola.js";
-import getEdgeWidths from "./../utils/getEdgeWidths";
+import getEdgeWidths from "./../utils/getEdgeWidths.js";
 import { getColaBipartiteCross } from "./../utils/getBipartiteCross.js";
 import getEdgePaths from "./../utils/getEdgePaths.js";
 import {
-  setColaConstraint,
   setCrossConstraint,
 } from "./../utils/constraints.js";
 
@@ -15,8 +14,7 @@ const colaConfluent = (
   bipartite,
   param,
   maxDepth,
-  hasEdgeColor = false,
-  outputForExp = false
+  isBaryWeighted,
 ) => {
   const lastLayer = 2 ** maxDepth;
   const cf = new Confluent(getQuasiBicliqueCover, param, maxDepth);
@@ -27,19 +25,17 @@ const colaConfluent = (
     cf.bipartitesAll
   );
 
-  console.error(edgeWidths, midNodeWidths);
-  console.error(cf);
+  // .error(edgeWidths, midNodeWidths);
+  // .error(cf);
 
-  // 座標決定process
   const width = 2750;
   const height = 1500;
   const d3cola = cola.d3adaptor(d3).linkDistance(300).size([width, height]);
-
-  // stress最小化
-  const layerGap = 150;
+  const layerGap = 200;
   const graph = makeGraphForCola(cf, layerGap);
-  // setColaConstraint(d3cola, graph, midNodeWidths, lastLayer);
+  
   // 重み付きエッジ交差数
+  // const weightedCross = setColaConstraint(d3cola, graph, midNodeWidths, lastLayer);
   const weightedCross = setCrossConstraint(
     bipartite,
     cf.bipartites,
@@ -47,18 +43,20 @@ const colaConfluent = (
     graph,
     midNodeWidths,
     edgeWidths,
-    d3cola,
-    lastLayer
+    lastLayer,
+    isBaryWeighted
   );
 
+  // stress最小化
   d3cola
     .nodes(graph.nodes)
     .links(graph.edges)
     .constraints(graph.constraints)
-    .symmetricDiffLinkLengths(40)
+    .symmetricDiffLinkLengths(50)
     .start(30, 40, 50);
 
-  console.error(graph);
+  // console.error(graph);
+  // console.error(edgeWidths)
 
   // エッジ交差数
   const cross = getColaBipartiteCross(cf.bipartites, graph.nodes);
@@ -76,11 +74,12 @@ const colaConfluent = (
   // パラメター、交差数、エッジ数、中間ノード数、誤差数のログ
   console.log("param", param);
   console.log("cross", cross);
+  console.log("weighted cross", weightedCross);
   console.log("edge number", totalEdgeCount);
   console.log("mid node", midNodesCount);
-  console.log("graph data", graph);
-  console.log("color", cf.bipartitesForColor);
-  console.log(edgePaths);
+  // console.log("graph data", graph);
+  // console.log("color", cf.bipartitesForColor);
+  // console.log(edgePaths);
 
   return {
     edgePaths,
--- src/node/colaConfluent2csv.js
+++ src/node/colaConfluent2csv.js
@@ -2,22 +2,72 @@ import colaConfluent from "../logic/colaConfluent.js";
 import fs from "fs";
 
 try {
-  const path = "public/act-mooc/json/mooc_actions_200.json";
-  const bipartite = JSON.parse(fs.readFileSync(path, "utf-8"));
-  const csvData = [["param", "depth","crosscount", "edge", "midnode", "missingedges"]];
-  for (let param = 10; param <= 100; param += 10) {
-    const { cross, totalEdgeCount, midNodesCount, missingEdges } = colaConfluent(bipartite, param / 100, 1, true);
-    csvData.push([param / 100, 1,cross, totalEdgeCount, midNodesCount, missingEdges]);
-  }
+  fs.readdir(
+    "public/random/json",
+    function (err, files) {
+      if (err) throw err;
+      console.log(files);
 
-  const csvContent = csvData.map((row) => row.join(",")).join("\n");
-  fs.writeFile(`public/csv/${path.split("/")[3]}.csv`, csvContent, (error) => {
-    if (error) {
-      console.error("CSVファイルの作成中にエラーが発生しました:", error);
-    } else {
-      console.log("CSVファイルが作成されました。");
+      for (let path of files) {
+        path = "public/random/json/" + path;
+        const bipartite = JSON.parse(fs.readFileSync(path, "utf-8"));
+        const csvData = [
+          ["param", "depth", "cross", "cross_bary", "weightedCross",  "weightedCross_bary", "percentage"],
+        ];
+
+      for(let depth = 2; depth <= 3; depth++) {
+        for (let param = 70; param <= 100; param += 10) {
+          let cross1 = 0;
+          let weightedCross1 = 0;
+          {
+            const { cross, weightedCross } =
+            colaConfluent(bipartite, param / 100, depth, false);
+            cross1 = cross;
+            weightedCross1 = weightedCross;
+          }
+          
+          let cross2 = 0;
+          let weightedCross2 = 0;
+          {
+            const { cross, weightedCross } =
+            colaConfluent(bipartite, param / 100, depth, true);
+            cross2 = cross;
+            weightedCross2 = weightedCross;
+          }     
+
+          if(weightedCross1 < 0.1) continue;
+          let elem = [
+            param / 100,
+            depth,
+            cross1,
+            cross2,
+            weightedCross1,
+            weightedCross2,
+            weightedCross2/weightedCross1
+          ];
+
+          csvData.push(elem);
+        }
+      }
+
+        const csvContent = csvData.map((row) => row.join(",")).join("\n");
+        fs.writeFile(
+          `public/random/csv/${path.split("/")[3]}.csv`,
+          csvContent,
+          (error) => {
+            if (error) {
+              console.error(
+                "CSVファイルの作成中にエラーが発生しました:",
+                error
+              );
+            } else {
+              console.log("CSVファイルが作成されました。");
+            }
+          }
+        );
+      }
     }
-  });
+  );
 } catch (e) {
   console.error(e);
 }
