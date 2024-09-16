8125fd81676d94b59649d79e4ed2c63d107c8781
kumastry <chma19063@g.nihon-u.ac.jp>
2024-09-17 07:19:21 +0900

    データの追加

--- src/node/rand2json.js
+++ src/node/rand2json.js
@@ -25,13 +25,15 @@ try {
     { recursive: true, withFileTypes: true },
     function (err, files) {
       if (err) throw err;
-      for (let depth = 3; depth <= 3; depth++) {
+      for (let depth = 1; depth <= 3; depth++) {
         for (let param = 65; param <= 100; param += 5) {
           let cnt = 0;
           let crossVals = new Array();
+          let wcrossVals = new Array();
           let edgeVals = new Array();
           let midVals = new Array();
           let missVals = new Array();
+          let missRVals = new Array();
           for (const file of files) {
             if (!file.isFile()) continue;
             console.log(
@@ -41,13 +43,21 @@ try {
             const bipartite = JSON.parse(
               fs.readFileSync(file.path + "/" + file.name, "utf-8")
             );
-            const { cross, totalEdgeCount, midNodesCount, missingEdges } =
-              colaConfluent(bipartite, param / 100, depth, true);
+            const {
+              cross,
+              weightedCross,
+              totalEdgeCount,
+              midNodesCount,
+              missConnectCount,
+              missConnectRatio,
+            } = colaConfluent(bipartite, param / 100, depth, true);
 
             crossVals.push(cross);
+            wcrossVals.push(weightedCross);
             edgeVals.push(totalEdgeCount);
             midVals.push(midNodesCount);
-            missVals.push(missingEdges);
+            missVals.push(missConnectCount);
+            missRVals.push(missConnectRatio);
             cnt++;
 
             if (cnt < instanceNum) continue;
@@ -66,9 +76,11 @@ try {
               },
               output: {
                 cross: crossVals,
+                wcross: wcrossVals,
                 edge: edgeVals,
                 mid: midVals,
                 miss: missVals,
+                missr: missRVals,
               },
               stat: {
                 cross: {
@@ -76,6 +88,12 @@ try {
                   median: calcMedian(crossVals),
                   max: Math.max(...crossVals),
                   min: Math.min(...crossVals),
+                },
+                wcross: {
+                  mean: calcMean(wcrossVals),
+                  median: calcMedian(wcrossVals),
+                  max: Math.max(...wcrossVals),
+                  min: Math.min(...wcrossVals),
                 },
                 edge: {
                   mean: calcMean(edgeVals),
@@ -95,20 +113,28 @@ try {
                   max: Math.max(...missVals),
                   min: Math.min(...missVals),
                 },
+                missr: {
+                  mean: calcMean(missRVals),
+                  median: calcMedian(missRVals),
+                  max: Math.max(...missRVals),
+                  min: Math.min(...missRVals),
+                },
               },
             });
 
             // 新しい配列objectを作成
             cnt = 0;
             crossVals = new Array();
+            wcrossVals = new Array();
             edgeVals = new Array();
             midVals = new Array();
             missVals = new Array();
+            missRVals = new Array();
           }
         }
       }
       fs.writeFileSync(
-        "public/random-exp/outputs_95_3.json",
+        "public/random-exp/outputs_95_4.json",
         JSON.stringify(output, null, 2)
       );
     }
@@ -116,7 +142,7 @@ try {
 } catch (e) {
   console.error(e);
   fs.writeFileSync(
-    "public/random-exp/outputs_95_3.json",
+    "public/random-exp/outputs_95_4.json",
     JSON.stringify(output, null, 2)
   );
 }
